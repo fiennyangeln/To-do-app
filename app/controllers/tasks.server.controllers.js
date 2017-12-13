8aac0c46653c7c77b4f2fb2d41fa7b7159dcceaa
pipiangelina <fifi.angelina@yahoo.com>
2017-12-14 01:59:49 +0800

    fix route, add update+delete function, add ngroute and ngresource

--- app/controllers/tasks.server.controllers.js
+++ app/controllers/tasks.server.controllers.js
@@ -1,26 +1,105 @@
 var Task = require('mongoose').model('Task');
 
+var getErrorMessage = function(err) {
+ if (err.errors) {
+   for (var errName in err.errors) {
+     if (err.errors[errName].message) return err.errors[errName].message;
+   }
+  }
+  else {
+    return 'Unknown server error';
+  }
+};
+
+module.exports.getOneById = function (req, res, next, id){
+  Task.findById(id).exec(function(err, task){
+    if (err){
+      return next(err);
+    }
+    if (!task){
+      return next(new Error ('Failed to load task'));
+    }
+    req.task=task;
+    next();
+  })
+};
+
+module.exports.read = function (req,res,next){
+  var task = req.task;
+  return res.json(task);
+};
+
 module.exports.create = function (req, res, next){
   var task = new Task(req.query);
   task.save(function(err){
     if (err){
-      return next(err);
+      return res.status(400).send({message: getErrorMessage(err)});
     }
     else {
       console.log(task);
       res.json(task);
     }
   })
-}
+};
 
 module.exports.list = function (req,res,next){
-  var query=Task.find().sort('-status');
+  var query=Task.find().sort('status');
   query.exec(function(err,tasks){
     if (err){
-      return next (err);
+      return res.status(400).send({message: getErrorMessage(err)});
     }
     else {
       res.json(tasks);
     }
   })
-}
+};
+
+module.exports.listNotDone = function (req,res,next){
+  var query = Task.find({'status':false}).sort('name');
+  query.exec(function(err,tasks){
+    if (err){
+      return res.status(400).send({message: getErrorMessage(err)});
+    }
+    else {
+      res.json(tasks);
+    }
+  })
+};
+
+module.exports.listDone = function (req,res,next){
+  var query = Task.find({'status': true}).sort('name');
+  query.exec(function(err,tasks){
+    if (err){
+      return res.status(400).send({message: getErrorMessage(err)});
+    }
+    else {
+      res.json(tasks);
+    }
+  })
+};
+
+module.exports.update = function (req,res,next){
+  var task = req.task;
+  task.name = req.body.name;
+  task.status = req.body.status;
+  task.save(function(err){
+    if (err){
+      return res.status(400).send({message: getErrorMessage(err)});
+    }
+    else{
+      res.json(task);
+    }
+  })
+};
+
+module.exports.remove = function (req,res,next){
+  var task = req.task;
+  task.remove(function(err){
+    if (err){
+      return res.status(400).send({message : getErrorMessage(err)});
+    }
+    else{
+      res.json(task);
+    }
+  })
+};
--- app/routes/tasks.server.routes.js
+++ app/routes/tasks.server.routes.js
@@ -1,5 +1,16 @@
 module.exports = function(app){
-  var users = require('../controllers/tasks.server.controllers');
-  app.get('/create',users.create);
+  var tasks = require('../controllers/tasks.server.controllers');
+  /*app.get('/create',users.create);
   app.get('/list',users.list);
+  app.get('/listNotDone', users.listNotDone);
+  */
+
+  app.get('/listNotDone', tasks.listNotDone);
+  app.get('/listDone', tasks.listDone);
+
+  app.route('/tasks').get(tasks.list).post(tasks.create);
+  app.route('/tasks/:taskId').get(tasks.getOneById, tasks.read).put(tasks.update).delete(tasks.remove);
+
+  app.param('taskId',tasks.getOneById);
+
 }
